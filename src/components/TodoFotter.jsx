353317c09517d63abf6177f7544ba6f33b3886a6
Butikov <butikov.exceedteam@gmail.com>
2022-03-02 18:00:47 +0300

    small changes

--- src/components/TodoFotter.jsx
+++ src/components/TodoFotter.jsx
@@ -6,39 +6,51 @@ const TodoFotter = ({
   completedLength,
   clearCompleted,
   filter,
-  setFilter
+  setFilter,
 }) => {
   return (
     <div className="d-flex justify-content-between px-3 border_footer">
       <div className="align-self-center">
         {!filter
-        ?`${itemsLength} ${filter} item${itemsLength === 1 ? '': 's'}${!itemsLength ? '': ' left'}`
-        :filter=='active'
-            ?`${activeLength} ${filter} item${activeLength === 1 ? '': 's'}${!activeLength ? '': ' left'}`
-            :`${completedLength} ${filter} item${completedLength === 1 ? '': 's'}${!completedLength ? '': ' left'}`}
+          ? `${itemsLength} ${filter} item${itemsLength === 1 ? "" : "s"}${
+              !itemsLength ? "" : " left"
+            }`
+          : filter == "active"
+          ? `${activeLength} ${filter} item${activeLength === 1 ? "" : "s"}${
+              !activeLength ? "" : " left"
+            }`
+          : `${completedLength} ${filter} item${
+              completedLength === 1 ? "" : "s"
+            }${!completedLength ? "" : " left"}`}
       </div>
       <div className="d-flex align-self-center justify-content-center">
         <div
           className={`align-self-center p-2 pointer buttonBorder ${
             !filter && " targeted"
           }`}
-          onClick={()=>{setFilter('')}}
+          onClick={() => {
+            setFilter("");
+          }}
         >
           All
         </div>
         <div
           className={`align-self-center p-2 pointer buttonBorder ${
-            filter=='active' && " targeted"
+            filter == "active" && " targeted"
           }`}
-          onClick={()=>{setFilter('active')}}
+          onClick={() => {
+            setFilter("active");
+          }}
         >
           Active
         </div>
         <div
           className={`align-self-center p-2 pointer buttonBorder ${
-            filter=='completed' && " targeted"
+            filter == "completed" && " targeted"
           }`}
-          onClick={()=>{setFilter('completed')}}
+          onClick={() => {
+            setFilter("completed");
+          }}
         >
           Completed
         </div>
